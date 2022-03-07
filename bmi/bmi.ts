7be890d6435814a1d56dc19f20e6af3946a7b192
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-03-07 10:24:01 +0100

    Exercises 9.1.-9.3. completed

--- bmi/bmi.ts
+++ bmi/bmi.ts
@@ -1,4 +1,22 @@
 type Result = string;
+interface DataValues {
+  weight: number
+  height: number
+}
+
+const parseArguments = (args: Array<string>): DataValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
 
 const calculateBmi = (height: number, weight: number): Result => {
   const bmi = weight / (height * height) * 10000;
@@ -15,4 +33,13 @@ const calculateBmi = (height: number, weight: number): Result => {
   }
 }
 
-console.log(calculateBmi(180, 74))
+try {
+  const { height, weight } = parseArguments(process.argv)
+  console.log(calculateBmi(height, weight));
+} catch (error) {
+  let errorMessage = 'Something bad happened. '
+  if (error instanceof Error) {
+    errorMessage += 'Error: ' + error.message
+  }
+  console.log(errorMessage);
+}
---
+++ calculator/exerciseCalculator.ts
@@ -0,0 +1,76 @@
+interface Result extends Rating {
+  periodLength: number
+  trainingDays: number
+  success: boolean
+  target: number
+  average: number
+}
+
+interface Rating {
+  rating: number
+  ratingDescription: string
+}
+
+const calculateExercises = (data: Array<number>, target: number): Result => {
+  const periodLength = data.length;
+  const trainingDays = data.filter(day => day > 0).length;
+  const average = data.reduce((partialSum, a) => partialSum + a, 0) / periodLength
+  const success = average > target
+  const { rating, ratingDescription } = { ...setRating(target, average) }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
+  }
+}
+
+const setRating = (target: number, average: number): Rating => {
+  let result: number = target / average
+  switch (true) {
+    case result < 1:
+      return { rating: 3, ratingDescription: 'well done!' }
+    case result > 1:
+      return { rating: 2, ratingDescription: 'not too bad but could be better' }
+    default:
+      return { rating: 1, ratingDescription: 'bad' }
+  }
+}
+
+const parseArgs = (args: Array<string>): { data: Array<number>, target: number } => {
+  let data = []
+  let target = 0
+
+  for (let index = 2; index < args.length; index++) {
+    if (!isNaN(Number(args[index]))) {
+      if (index === 2) {
+        target = Number(args[index])
+      } else {
+        data.push(Number(args[index]))
+      }
+
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+  }
+  console.log(data, target);
+
+
+  return { data, target }
+}
+
+try {
+  const { data, target } = parseArgs(process.argv)
+  console.log(calculateExercises(data, target))
+} catch (error) {
+  let errorMessage = 'Something bad happened. '
+  if (error instanceof Error) {
+    errorMessage += 'Error: ' + error.message
+  }
+  console.log(errorMessage);
+}
+
